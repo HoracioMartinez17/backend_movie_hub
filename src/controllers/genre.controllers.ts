d9408f579c66ecf77eefacc6cebf086d47cba079
Horacio Martinez <litstyle17@gmail.com>
2023-09-05 13:54:38 +0200

    updating comments from Spanish to English

--- src/controllers/genre.controllers.ts
+++ src/controllers/genre.controllers.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from "express";
 import {prismaClient} from "../db/clientPrisma";
+import { convertToType } from "../utils/convertToType";
 
-
+// Create a new genre
 export const createGenre = async (req: Request, res: Response) => {
     const { name } = req.body;
 
     try {
-        // Verificar si el género ya existe
+        // Check if the genre already exists
         const existingGenre = await prismaClient.genres.findFirst({ where: { name } });
 
         if (existingGenre) {
             return res.status(400).send({ error: 'Genre already exists' });
         }
 
-        // Crear el nuevo género
+        // Create a new genre
         const newGenre = await prismaClient.genres.create({ data: { name } });
 
         res.status(201).send({ status: 'success', message: 'Genre created successfully', newGenre });
@@ -23,7 +24,7 @@ export const createGenre = async (req: Request, res: Response) => {
     }
 };
 
-
+// Get movies by genre and user
 export const getMoviesByGenreAndUser = async (req: Request, res: Response) => {
     const { genreName, userId } = req.params;
     const pageSize = 4; // Number of movies per page
@@ -35,7 +36,7 @@ export const getMoviesByGenreAndUser = async (req: Request, res: Response) => {
             where: { name: genreName },
             include: {
                 movies: {
-                    where: { userId },
+                    where: { id: convertToType(userId)},
                     skip: skip,
                     take: pageSize,
                     include: {
@@ -59,7 +60,7 @@ export const getMoviesByGenreAndUser = async (req: Request, res: Response) => {
             return res.status(404).send({ status: 'error', error: "Genre not found" });
         }
 
-        const totalMovies = await prismaClient.movies.count({ where: { genreId: genre.id, userId } });
+        const totalMovies = await prismaClient.movies.count({ where: { genreId:  convertToType(genre.id), userId } });
         const totalPages = Math.ceil(totalMovies / pageSize);
 
         res.status(200).send({
@@ -78,11 +79,7 @@ export const getMoviesByGenreAndUser = async (req: Request, res: Response) => {
     }
 };
 
-
-
-
-
-
+// Get all genres
 export const getAllGenres = async (req: Request, res: Response) => {
     const { userId } = req.params;
 
@@ -91,7 +88,7 @@ export const getAllGenres = async (req: Request, res: Response) => {
         const allGenres = await prismaClient.genres.findMany({
             include: {
                 movies: {
-                    where: { userId },
+                    where: { id: convertToType(userId) },
                     select: {
                         id: true,
                         title: true,
@@ -104,10 +101,59 @@ export const getAllGenres = async (req: Request, res: Response) => {
             }
         })
 
-
         res.status(201).send(allGenres)
 
     } catch (error) {
         res.status(500).send(error)
     }
 }
+
+// Update a genre's name
+export const updateGenre = async (req: Request, res: Response) => {
+    const { id } = req.params; // Genre ID to be updated
+    const { name } = req.body; // New name for the genre
+
+    try {
+        // Find the genre by its ID and update its name
+        const updatedGenre = await prismaClient.genres.update({
+            where: { id:  convertToType(id) },
+            data: { name }
+        });
+
+        // Check if the genre was updated
+        if (!updatedGenre) {
+            return res.status(404).send({ status: 'error', message: 'Genre not found' });
+        }
+
+        // Send a successful response with the updated genre
+        res.status(200).send({ status: 'success', message: 'Genre updated successfully', updatedGenre });
+    } catch (err) {
+        console.error(err); // Log the error to the console for debugging purposes
+        // In case of an internal error, return an error message with status code 500
+        res.status(500).send({ error: 'Internal server error' });
+    }
+};
+
+// delete genre
+export const deleteGenre = async (req: Request, res: Response) => {
+    const { id } = req.params; // Genre ID to be deleted
+
+    try {
+        // Find the genre by its ID and delete it
+        const deletedGenre = await prismaClient.genres.delete({
+            where: { id: convertToType(id) }
+        });
+
+        // Check if the genre was deleted
+        if (!deletedGenre) {
+            return res.status(404).send({ status: 'error', message: 'Genre not found' });
+        }
+
+        // Send a successful response with no content
+        res.status(204).send({ status: 'success', message: 'Genre deleted successfully' });
+    } catch (err) {
+        console.error(err); // Log the error to the console for debugging purposes
+        // In case of an internal error, return an error message with status code 500
+        res.status(500).send({ error: 'Internal server error' });
+    }
+};
